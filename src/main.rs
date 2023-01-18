2ad1586087d73e3e4a6491288df5ab85add78841
Gilberto Asuaje <gilberto.asuaje@epayco.com>
2023-01-17 22:16:30 -0500

    feat: build connection with database

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use std::env;
+use dotenvy::dotenv;
 use actix_web::{App, HttpServer};
-
+use diesel::PgConnection;
+use diesel::r2d2::{ConnectionManager, Pool};
 mod utils;
 mod tweets;
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("it can't connect to database");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = Pool::builder().build(manager).expect("it can't build pool");
+    HttpServer::new( move || {
         App::new()
+        .app_data(pool.clone())
         .service(tweets::get_tweet_by_id)
         .service(tweets::create_tweet)
     })
